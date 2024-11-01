eb8178e8ec7072380c820779590b5ddf5a004617
Henry Acevedo <brunokane7@yahoo.com>
2024-11-01 15:00:25 -0500

    Updated connection string.

--- Models/MovietheaterContext.cs
+++ Models/MovietheaterContext.cs
@@ -19,7 +19,8 @@ public partial class MovietheaterContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-R601H3RA\\SQLEXPRESS;Database=movietheater;Trusted_Connection=true;TrustServerCertificate=true;Persist Security Info=true");
+        //=> optionsBuilder.UseSqlServer("Server=LAPTOP-R601H3RA\\SQLEXPRESS;Database=movietheater;Trusted_Connection=true;TrustServerCertificate=true;Persist Security Info=true");
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-R601H3RA;Database=movietheater;Trusted_Connection=true;TrustServerCertificate=true;Persist Security Info=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //new
-builder.Services.AddDbContext<MovietheaterContext>(options => options.UseSqlServer("Server=LAPTOP-R601H3RA\\SQLEXPRESS;Database=movietheater;Trusted_Connection=true;TrustedServerCertificate=true;Persist Security Info=true"));
+//builder.Services.AddDbContext<MovietheaterContext>(options => options.UseSqlServer("Server=LAPTOP-R601H3RA\\SQLEXPRESS;Database=movietheater;Trusted_Connection=true;TrustedServerCertificate=true;Persist Security Info=true"));
+builder.Services.AddDbContext<MovietheaterContext>(options => options.UseSqlServer("Server=LAPTOP-R601H3RA;Database=movietheater;Trusted_Connection=true;TrustedServerCertificate=true;Persist Security Info=true"));
 
 var app = builder.Build();
 
