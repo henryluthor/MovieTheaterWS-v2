f2743cd03b23a2c700c81c08efbc368b5d52b353
Henry Acevedo <brunokane7@yahoo.com>
2025-10-06 11:46:55 -0500

    Implemented moviesByComplex method.

--- Controllers/ComplexMovieController.cs
+++ Controllers/ComplexMovieController.cs
@@ -22,12 +22,28 @@ namespace MovieTheaterWS_v2.Controllers
             return await _context.ComplexMovies.ToListAsync();
         }
 
-        [HttpGet("{idComplex}")]
-        public async Task<List<ComplexMovie>> GetMoviesByComplex(int idComplex)
-        {
-            var allComplexMovies = from cm in _context.ComplexMovies select cm;
+        //This method only returns IDs, not very user friendly
+        //[HttpGet("{idComplex}")]
+        //public async Task<List<ComplexMovie>> GetMoviesByComplex(int idComplex)
+        //{
+        //    var allComplexMovies = from cm in _context.ComplexMovies select cm;
 
-            return await allComplexMovies.Where(cm => cm.IdComplex == idComplex).ToListAsync();
+        //    return await allComplexMovies.Where(cm => cm.IdComplex == idComplex).ToListAsync();
+        //}
+
+        // This method need protection against SQL injection
+        [HttpGet("{idComplex}")]
+        public async Task<List<Movie>> GetMoviesByComplex(int idComplex)
+        {
+            //int idComplexToSearch = idComplex;
+            var moviesByComplex = await _context.Movies.FromSqlInterpolated(
+                $@"select m.idMovie, m.Title, m.Year, m.Runtime, m.Genre, m.IMDBId 
+from ComplexMovie cm 
+join Movie m on cm.idMovie = m.idMovie 
+where cm.idComplex = {idComplex}"
+                ).ToListAsync();
+            
+            return moviesByComplex;
         }
     }
 }
